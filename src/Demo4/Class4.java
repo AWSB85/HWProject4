e9f4bdaf9589f3beda2ea5dc9c04a00481747c76
awsbasiya <awsbasiya@localhost>
2023-04-06 17:11:50 -0400

    Class4 commit

---
+++ src/Demo4/Class4.java
@@ -0,0 +1,18 @@
+package Demo4;
+
+public class Class4 {
+	
+	
+public static void main (String [] args) {
+		
+		System.out.println("Class4........");
+		
+		System.out.println("Git Class4.....");
+		
+		
+	}
+
+
+	
+
+}
